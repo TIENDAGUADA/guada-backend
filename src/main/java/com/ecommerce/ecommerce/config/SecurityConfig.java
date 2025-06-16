b452fcb9786fc019977d262afa80f83801d32318
LeandroZacariaz <leandrozacariaz18@gmail.com>
2025-06-16 14:37:43 -0300

    permitiendo nueva url

--- src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:5173, https://tienda-guada.vercel.app/"
+            "http://localhost:5173, https://tienda-jope.vercel.app/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
